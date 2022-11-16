9b6339f9654086fb9d8b208aaf2ff295efe9e6a0
Malik Salem <malik.salem@mths.ca>
2022-11-16 16:04:29 +0000

    code

---
+++ factoral_while_loop.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# Created by: maliksalem1
+# Created on: Oct 2022
+# This program uses while loops
+
+
+def main():
+
+    loop_counter = 0
+    multiply_int = 1
+
+    # This function multiplies all the whole numbers up to the inputted number
+
+    # Input
+    positive_string = input("Please enter a positive number: ")
+    print("")
+
+    # Process and output
+    try:
+        positive_integer = int(positive_string)
+        if positive_integer >= 0:
+            while loop_counter < positive_integer:
+                loop_counter = loop_counter + 1
+                multiply_int = multiply_int * loop_counter
+            print("{0}! = {1}.".format(positive_integer, multiply_int))
+        else:
+            print("{0} is not a positive integer".format(positive_string))
+    except ValueError:
+        print("{0} is not a valid input".format(positive_string))
+    finally:
+        print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
